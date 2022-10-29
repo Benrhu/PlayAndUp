b4770ae14ebfef04c438efeb9ee4283f6f7b8e45
Rubdh <Benrhu@gmail.com>
2022-10-29 21:06:19 +0200

    JavaScript works!

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,50 @@
-document.querySelectorAll('.flip-card-click .flip-card-inner').forEach(function(item) {  
-    item.addEventListener('keypress', function(evt) { if (evt.keyCode == 13 || evt.keyCode == 32) { item.click(); } });
-});
+/**
+ * In the first four rounds, the user can earn up to 25 pts per card.
+ * 
+ * From round 5 to 10, you can earn up to 50 pts per card.
+ * 
+ * Up to 15 round, you can earn up to 100 pts per card.
+ */
 
-document.querySelectorAll('.flip-card-click').forEach(function(item) { 
-    item.addEventListener('click', function () { this.classList.toggle('flipped');  });
+var highScore;
+var yourPoints = Number(yourPoints);
+
+var rndm = Math.floor(Math.random() * 25);
+
+var card = document.querySelectorAll('.flip-card-click')
+
+/* card.addEventListener('click', () => {
+  rndm += yourPoints;
+  document.getElementById('yourPoints').innerHTML = yourPoints;
+}); */
+
+function addPoints() {
+  yourPoints = rndm;
+  document.getElementById('yourPoints').innerHTML = yourPoints;
+  console.log(yourPoints);
+  console.log(rndm);
+}
+
+function fade(){
+  document.querySelectorAll('.flip-card-click').forEach(function (item) {
+  item.classList.toggle('fade');
+});
+}
+
+function reset() {
+  document.querySelectorAll('.flip-card-click').forEach(function (item) {
+    item.classList.remove('fade');
+  });
+}
+
+
+document.querySelectorAll(".flip-card-click").forEach(function (item) {
+  item.addEventListener("click", function () {
+    this.classList.toggle("flipped");
+    addPoints();
+    fade();
+    setTimeout(() => {
+      reset();
+    }, 600);
+  });
 });
